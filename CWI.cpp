d38daa94e0843a441e5f0756c3ad9715ad0cf1c1
Alex Esclamado <aesclamadoca@gmail.com>
2021-12-17 17:41:20 +0000

    first commit

---
+++ CWI.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int c = 4;
+  cout <<  42*c;
+  return 0;
+  }
+
+
+class MyClass{
+    string name, classname, haircolor;
+  public:
+    MyClass(
+
+  private:
+    
+}
