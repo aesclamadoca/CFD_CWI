acbf6b752c0a1865559b58f23ea2c28bcf933296
Alex Esclamado <aesclamadoca@gmail.com>
2021-12-26 12:34:35 +0100

    Add class file and finish all 3 ds

--- CWI.py
+++ CWI.py
@@ -1,11 +1,27 @@
-import sys
+import solver
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
+import os
+dir='Plots'
+if not os.path.exists(dir):
+    os.makedirs(dir)
+# Run -defaults write org.python.python ApplePersistenceIgnoreState NO- before executing -python3 <filename>- if running from mac terminal
+
+
 
 def comp_phi_plot(scheme):
+
   solve = solver(scheme)
+  print(f"""Domain properties are defined as: 
+          \t Gamma_phi = {solve.get_Gamma()}
+          \t Fluid Velocity = {solve.get_u()} m/s
+          \t Fluid Density = {solve.get_rho()} kg/m^3
+          \t Number of Gridpoints = {solve.get_gridpoint_n()}""")
+  print('Solving equation in the Domain ...')
+
+  # Solve for phi
   analytical_phi = solve.get_analytical_phi()
   if scheme == "CDS": numerical_phi = solve.get_numerical_phi()
   elif scheme == "UDS": numerical_phi = solve.UDS()
@@ -14,6 +30,8 @@ def comp_phi_plot(scheme):
     print("Please enter a valid discrtization scheme acronym")
     return
 
+  # Plot phi
+  fig = plt.figure()
   plt.plot(solve.get_x(),analytical_phi,label='Analytical Solution')
   plt.plot(solve.get_x(),numerical_phi,label=f'Numerical Solution ({scheme})')
 
@@ -21,85 +39,16 @@ def comp_phi_plot(scheme):
   plt.xlabel('x')
   plt.ylabel(r'$\phi$')
   plt.legend()
-  plt.show(block=True)
-class solver():
-
-  def __init__(self,
-              scheme_acronym, 
-              fluid_velocity = 2,
-              fluid_density = 0.5,
-              Gamma = 0.5,
-              domain_L = 1,
-              inlet_bc = 100,
-              outlet_bc = 20,
-              grid_spacing = 1e-2
-              ):
-    
-    # Global Attributes
-    self.scheme = scheme_acronym
-    self.u = fluid_velocity
-    self.rho = fluid_density
-    self.Gamma = Gamma
-    self.L = domain_L
-    self.phi_0 = inlet_bc
-    self.phi_L = outlet_bc
-    self.dx = grid_spacing
-    self.global_Pe = self.rho * self.u * self.L / self.Gamma
-    self.local_Pe = self.rho * self.u * self.dx / self.Gamma
-    self.x = np.array([i*self.dx for i in range(int(self.L/self.dx))])
-    self.analytical_phi = np.zeros(int(self.L/self.dx))
-    self.phi = np.zeros(int(self.L/self.dx))
-
-    # CDS phi and Coefficients for constant u,dx,Gamma
-    self.phi = np.zeros(int(self.L/self.dx))
-    self.P = np.zeros(int(self.L/self.dx))
-    self.Q = np.zeros(int(self.L/self.dx))
-    self.a = 2*self.Gamma / self.dx
-    self.b = self.Gamma / self.dx - 0.5 * self.rho * self.u
-    self.c = self.Gamma / self.dx + 0.5 * self.rho * self.u
-
-    #Solve for phi
-    if self.scheme == "CDS": self.CDS()
-    elif self.scheme == "UDS": self.UDS()
-    elif self.scheme == "PLDS": self.PLDS()
-    self.analytical_soln()
-
-  # Getter methods
-  def get_local_Pe(self): return self.local_Pe
-  def get_x(self): return self.x
-  def get_P(self): return self.P
-  def get_Q(self): return self.Q
-  def get_numerical_phi(self): return self.phi
-  def get_analytical_phi(self): return self.analytical_phi
-  
-  ## Use Linear Algebraic Equation Solution Algorithm TDMA
-  
-  def CDS(self):
-    # Forward Pass to calculate P and Q
-    self.P[0] = 0
-    self.Q[0] = self.phi_0 
-    self.Q[-1] = self.phi_L
-    for i in range(1,len(self.x)-1):
-      self.P[i] = self.b / (self.a - self.P[i-1])
-      self.Q[i] = self.Q[i-1] / (self.a - self.P[i-1])
-      print
-    # Backward Pass to calculate phi
-    self.phi[-1] = self.Q[-1]
-    for i in [y for y in range(1,len(self.x)-1)][::-1]:
-      self.phi[i-1] = self.P[i] * self.phi[i+1] + self.Q[i]
-
-  # def UDS():
-  
-  # def PLDS():
-
-  def analytical_soln(self):
-    self.analytical_phi = self.phi_0 + (np.exp(self.global_Pe * self.x/self.L)-1)/(np.exp(self.global_Pe)-1) * (self.phi_L-self.phi_0)
+  # plt.show()
+  plt.savefig(f'{dir}/{scheme}',dpi=800)
+  print(f"Plot of Distribution of phi along the domain was saved to {dir}/{scheme}\n\n")
 
 
 if __name__ == '__main__':
+
   scheme_dict = {'CDS':'Central Differencing Scheme',
-               'UDS':'Upwind Differencing Scheme',
-               'PLDS':'Power Law Differencing Scheme'}
+                'UDS':'Upwind Differencing Scheme',
+                'PLDS':'Power Law Differencing Scheme'}
 
   end_condition = False
   while not end_condition:
---
+++ solver.py
@@ -0,0 +1,118 @@
+class solver():
+
+  def __init__(self,
+              scheme_acronym, 
+              fluid_velocity = 1,
+              fluid_density = 0.5,
+              Gamma = 0.5,
+              domain_L = 1,
+              inlet_bc = 100,
+              outlet_bc = 20,
+              gridpoints = 10
+              ):
+    
+    # Global Attributes
+    self.scheme = scheme_acronym
+    
+    self.u = fluid_velocity
+    self.rho = fluid_density
+    self.Gamma = Gamma
+    self.L = domain_L
+    self.dx = self.L/gridpoints
+    self.global_Pe = self.rho * self.u * self.L / self.Gamma
+    self.local_Pe = self.rho * self.u * self.dx / self.Gamma
+    
+    self.x = np.array([i*self.dx for i in range(int(self.L/self.dx))])
+    self.phi_0 = inlet_bc
+    self.phi_L = outlet_bc
+    
+    self.analytical_phi = np.zeros(10*len(self.x))
+    self.phi = np.zeros(int(self.L/self.dx))
+    self.P = np.zeros(int(self.L/self.dx))
+    self.Q = np.zeros(int(self.L/self.dx))
+    self.error = 0
+
+    #Solve for phi
+    if self.scheme == "CDS": self.CDS()
+    elif self.scheme == "UDS": self.UDS()
+    elif self.scheme == "PLDS": self.PLDS()
+    self.analytical_soln()
+    self.numerical_error()
+
+  # Getter methods
+  def get_local_Pe(self): return self.local_Pe
+  def get_x(self): return self.x
+  def get_P(self): return self.P
+  def get_Q(self): return self.Q
+  def get_numerical_phi(self): return self.phi
+  def get_analytical_phi(self): return self.analytical_phi
+  def get_rho(self): return self.rho
+  def get_u(self): return self.u
+  def get_Gamma(self): return self.Gamma
+  def get_gridpoint_n(self): return int(self.L/self.dx)
+  def get_error(self): return self.error
+
+
+  # Discretization Schemes
+  def CDS(self):
+    D = self.Gamma / self.dx
+    F = self.rho * self.u
+    #Coefficients
+    a = 2*D
+    b = D - 0.5 * F
+    c = D + 0.5 * F
+
+    self.TDMA(a,b,c,0)
+  
+  def UDS(self):
+    D = self.Gamma / self.dx
+    F = self.rho * self.u
+    #Coefficients
+    a = 2*D + max(F,0) + max(-F,0)
+    b = D + max(-F,0)
+    c = D + max(F,0)
+  
+    self.TDMA(a,b,c,0)
+
+  def PLDS(self):
+    D = self.Gamma / self.dx
+    F = self.rho * self.u
+    #Coefficients
+    b = D*max((1-0.1*self.local_Pe)**5,0) + max(-F,0)
+    c = D*max((1-0.1*self.local_Pe)**5,0) + max(F,0)
+    a = b + c
+  
+    self.TDMA(a,b,c,0)
+
+
+
+
+  ## Use Linear Algebraic Equation Solution Algorithm TDMA
+  def TDMA(self,a,b,c,d):
+    # Set BCs
+    self.P[0], self.P[-1] = 0,0
+    self.Q[0], self.Q[-1] = self.phi_0, self.phi_L
+
+    # Forward Pass to calculate P and Q
+    for i in [x+1 for x in range(len(self.x)-2)]:
+      self.P[i] = b / (a - c*self.P[i-1])
+      self.Q[i] = c*self.Q[i-1] / (a - c*self.P[i-1])
+
+    # Backward Pass to calculate phi
+    self.phi[-1] = self.Q[-1]
+    for i in [y for y in range(len(self.x)-1)][::-1]:
+      self.phi[i] = self.P[i] * self.phi[i+1] + self.Q[i]
+  
+
+  
+
+  def analytical_soln(self):
+    x_domain = np.array([x/10/len(self.x) for x in range(10*len(self.x))])
+    self.analytical_phi = self.phi_0 + (np.exp(self.global_Pe * x_domain/self.L)-1)/(np.exp(self.global_Pe)-1) * (self.phi_L-self.phi_0)
+
+  def numerical_error(self):
+    for i in range(len(self.phi)):
+      self.error += 100*self.dx/self.L * (self.phi[i]-self.analytical_phi[10*i])/self.analytical_phi[10*i]
+    
+
+  
